f547d56ba0d4e99950779723f7d3e0afede81a1e
Ash Ryan <ash@ashryanbeats.com>
2015-03-14 17:20:04 -0400

    Notifications now working for user info initialization and changes.

--- app.js
+++ app.js
@@ -9,16 +9,13 @@ app.get('/', function(req, res) {
 });
 
 io.on('connection', function(socket) {
-	var msgNewConnection = 'A new user connected!';
+  var userId = socket.id;
 	var msgNewDissconnection = 'Someone disconnected ; ;';
 	
-  io.emit('client-register-id', socket.id);
-  io.emit('new-connection', msgNewConnection);
-	console.log(msgNewConnection, socket.id);
-	
+  io.emit('client-register-id', userId);
 	
 	socket.on('disconnect', function(){
-    	console.log('user disconnected', socket.id);
+    	console.log('user disconnected', userId);
     	io.emit('new-disconnection', msgNewDissconnection);
       delete people[socket.id];
       console.log("People logged in after user disconnect");
@@ -26,7 +23,42 @@ io.on('connection', function(socket) {
   });
 
 	socket.on('update-user-info', function(user) {
-		people[socket.id] = user;
+    var msgNewConnection;
+    var nameChange = false;
+    var emotionChange = false;
+
+    // If the user doesn't exist
+    if(people.hasOwnProperty(userId) === false) {
+      // Write the data recieved from the client to the people obj at the property named userId (socket.id)
+      people[userId] = user;
+      msgNewConnection = people[userId].name + ' connected and is feeling ' + people[userId].emotion + "!";
+    }
+    // Else find out what has changed
+    else {
+      if (people[userId].name !== user.name) {
+        nameChange = true;
+      } 
+      if (people[userId].emotion !== user.emotion) {
+        emotionChange = true;
+      }
+    }
+
+    // Set the message to be emitted based on what has changed. The null case is handled in the client script.
+    if (nameChange && emotionChange) {
+      msgNewConnection = people[userId].name + " changed names to " + user.name + " and is now feeling " + user.emotion + "!";
+      people[userId] = user;
+    }
+    else if (nameChange) {
+      msgNewConnection = people[userId].name + " changed names to " + user.name + "!"; 
+      people[userId] = user;
+    }
+    else if (emotionChange) {
+      msgNewConnection = people[userId].name + " is now feeling " + user.emotion + "!";
+      people[userId] = user;
+    }
+
+
+    io.emit('new-connection', msgNewConnection);
 		console.log("People logged in: ");
     console.log(people);
 	});
